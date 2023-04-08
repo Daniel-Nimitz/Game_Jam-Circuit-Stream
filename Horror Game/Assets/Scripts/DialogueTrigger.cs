b3b2f6cf71e5657f4969e03724e4157f31586578
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2023-04-08 10:21:22 -0600

    Single Dialogue Implemented
    Dialogue now appears when character gets close.  Only one dialogue per character supported.

--- Horror Game/Assets/Scripts/DialogueTrigger.cs
+++ Horror Game/Assets/Scripts/DialogueTrigger.cs
@@ -5,18 +5,46 @@ using TMPro;
 
 public class DialogueTrigger : MonoBehaviour
 {
+    public GameObject playerCharacter;
+
+
+
     public TMP_Text dialogueText;
 
     public string[] dialogueToSay;
+
+    private float distanceToPlayer;
+
+    public float startingDialogueDistance = 40;
+
+
+    private void Awake()
+    {
+        playerCharacter = GameObject.FindWithTag("Player");
+    }
+
+
+    private void Update()
+    {
+        distanceToPlayer = Vector3.Distance(this.gameObject.transform.position, playerCharacter.transform.position);
+        if (distanceToPlayer <  startingDialogueDistance) {
+            dialogueText.gameObject.SetActive(true);
+            dialogueText.text = dialogueToSay[0];
+        }
+    }
+
+    
+    
+
+
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("collision happened with trigger");
         dialogueText.gameObject.SetActive(true);
         dialogueText.text = dialogueToSay[0];
-        foreach(string i in dialogueToSay){ 
         
-        }
     }
-
+    //Use coroutines to say each text one at a time
+    //Make if statement to check only for player
 }
