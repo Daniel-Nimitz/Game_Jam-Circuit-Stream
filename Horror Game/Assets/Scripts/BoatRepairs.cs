0488f5d9289faca902a365a3781890221dbce977
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2023-04-08 18:18:52 -0600

    Engine and Gas Boat Attachments
    Made engine and gas both add special attributes to boat for better feedback to player.
    -Engine now activates inactive engine object on boat when added
    -Propeller on boat now rotates after fuel is added

--- Horror Game/Assets/Scripts/BoatRepairs.cs
+++ Horror Game/Assets/Scripts/BoatRepairs.cs
@@ -13,19 +13,24 @@ public class BoatRepairs : MonoBehaviour
 
 
     public GameObject engineOnBoat;
+    public Animator boatPropellerAnimator;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Gas"))
         {
             hasGas = true;
+            Destroy(other.gameObject);
+            boatPropellerAnimator.enabled = true;
             Debug.Log("Gas Can Ran into Boat Trigger");
         }
         if (other.CompareTag("Compass")) {
             hasCompass = true;
+            Destroy(other.gameObject);
             Debug.Log("Compass Ran Into Boat Trigger");
         }
         if(other.CompareTag("Engine")) {
             hasEngine = true;
+            Destroy(other.gameObject);
             engineOnBoat.SetActive(true);
             Debug.Log("Engine Ran into Boat Trigger");
 
